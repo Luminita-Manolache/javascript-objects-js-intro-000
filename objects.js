288cd05fa0a2b15dd68796c42652029c9478a1c4
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-02-01 07:08:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
    return Object.assign(playlist, {'Phil Ochs': 'Here\'s to the State of Mississippi' })
 }
+
+function removeFromPlaylist(playlist, artistName) {
+
+  delete playlist.'Slowdive';
+}
