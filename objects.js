0a8552b627188e4127a5eaeae47230d0e8e4672e
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-02-01 06:30:48 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,6 @@ var playlist = {
   artistName: 'songTitle'
 };
 function updatePlaylist(playlist, artistName, songTitle) {
-
+   Object.assign({}, {Slowdive: 'Alison' })
+   Object.assign({}, {My Bloody Valentine: 'Sometimes' })
 }
