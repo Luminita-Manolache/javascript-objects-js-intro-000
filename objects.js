23723455dd103205e70e96e32b872fb238ceb87e
Luminita Manolache <luminitalumi.manolache@gmail.com>
2020-02-01 06:42:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,10 @@
-var playlist = {
-  'Slowdive': 'Alison',
-  'My Bloody Valentine': 'Sometimes'
-};
+
 
 function updatePlaylist(playlist, artistName, songTitle) {
+  var playlist = {
+    'Slowdive': 'Alison',
+    'My Bloody Valentine': 'Sometimes'
+  };
+  
    Object.assign({'Phil Ochs': 'Here\'s to the State of Mississippi' })
 }
